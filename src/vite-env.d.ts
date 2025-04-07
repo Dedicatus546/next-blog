5822cb3f8fa5e7320bd44042e4084d7fedcbba01
Par9uet <1607611087@qq.com>
2025-04-07 17:20:48 +0800

    chore: env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,14 @@
 /// <reference types="vite/client" />
 /// <reference types="unplugin-vue-router/client" />
+
+interface ImportMetaEnv {
+  readonly GITHUB_CLIENT_ID: string;
+  readonly GITHUB_CLIENT_SECRET: string;
+  readonly GITHUB_REPO: string;
+  readonly GITHUB_OWNER: string;
+  // 更多环境变量...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
