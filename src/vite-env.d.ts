6f04b0c42208a75b217f4b9e118259990f36b32a
Par9uet <1607611087@qq.com>
2025-04-15 17:23:29 +0800

    chore: env 写死 repo id

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,6 +5,7 @@ interface ImportMetaEnv {
   readonly GITHUB_CLIENT_ID: string;
   readonly GITHUB_CLIENT_SECRET: string;
   readonly GITHUB_REPO: string;
+  readonly GITHUB_REPO_ID: string;
   readonly GITHUB_OWNER: string;
   readonly P9_PAGESIZE: string;
   // 更多环境变量...
