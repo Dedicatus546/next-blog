3065d5c7a775d4e6f1c6877e586604d94606126f
Par9uet <1607611087@qq.com>
2025-04-16 15:40:26 +0800

    feat: footer 随机语录

--- src/components/MujikaFooter.vue
+++ src/components/MujikaFooter.vue
@@ -1,4 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const route = useRoute();
+const randomSignList = ref([
+  {
+    content:
+      "如果我和狗一样有尾巴的话，一定会藏不住这份喜悦，而尾巴一直摇个不停吧。",
+    from: "《秒速五厘米》",
+  },
+  {
+    content: "見えない感情が押し寄せるたびに 幸せになっていいのだろうかと",
+    from: "《Wave》（我的妻子没有感情）",
+  },
+  {
+    content: "あたしが隣にいるうちに 愛をもっとあたしにください",
+    from: "《あたしが隣にいるうちに》（顿之勇者成名录）",
+  },
+  {
+    content:
+      "我们总这样重复分离 却要重新开始 相互送别对方 说着来世再见 再次失忆着相聚",
+    from: "《我记得》",
+  },
+]);
+const randomSignIndex = ref(0);
+watch(
+  () => route.name,
+  () => {
+    randomSignIndex.value = Math.floor(
+      Math.random() * randomSignList.value.length,
+    );
+  },
+);
+</script>
 
 <template>
   <footer
@@ -11,10 +42,11 @@
     items-center
   >
     <div flex items-center>
-      <span>Power by&nbsp;</span>
+      <span>由&nbsp;</span>
       <i class="i-logos:vue"></i>
       <span>&nbsp;+&nbsp;</span>
       <i class="i-logos:vitejs"></i>
+      <span>&nbsp;驱动</span>
     </div>
     <div text-center>
       <a
@@ -26,7 +58,9 @@
       2021 - PRESENT © Dedicatus545
     </div>
     <div text="[var(--mygo-c-text-2)]" text-center>
-      如果我和狗一样有尾巴的话，一定会藏不住这份喜悦，而尾巴一直摇个不停吧。
+      {{ randomSignList[randomSignIndex].content }}
+      -
+      {{ randomSignList[randomSignIndex].from }}
     </div>
   </footer>
 </template>
