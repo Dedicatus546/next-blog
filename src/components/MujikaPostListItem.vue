c8dd7c2b813143a9ed5204e2c8b6617d3b8b2996
Par9uet <1607611087@qq.com>
2025-04-18 17:30:13 +0800

    refactor:  调整列表标题样式

--- src/components/MujikaPostListItem.vue
+++ src/components/MujikaPostListItem.vue
@@ -12,9 +12,25 @@ defineProps<{
 <template>
   <MujikaCard :padding-level="2">
     <div flex="~ col" gap-4>
-      <RouterLink :to="post.path">
-        <h2 break-all text="34px center">{{ post.title }}</h2>
-      </RouterLink>
+      <h1 text="30px center">
+        <RouterLink
+          :to="post.path"
+          break-all
+          relative
+          before:mx-auto
+          before:h-2px
+          before:w-0
+          before:content-empty
+          before:transition
+          before:inset-x-0
+          before:bottom--5px
+          before:absolute
+          before:hover:w-full
+          before:hover:bg="[var(--mygo-c-text-1)]"
+        >
+          {{ post.title }}
+        </RouterLink>
+      </h1>
       <MujikaPostMeta :page="post" />
       <div class="kan-doc" v-html="post.excerpt"></div>
     </div>
