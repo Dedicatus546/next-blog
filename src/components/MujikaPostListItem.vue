ddc4556fd3b681060a232d7907ab2678b0025ab4
Par9uet <1607611087@qq.com>
2025-04-18 23:04:28 +0800

    refactor: 调整列表页列表项标题样式

--- src/components/MujikaPostListItem.vue
+++ src/components/MujikaPostListItem.vue
@@ -15,19 +15,24 @@ defineProps<{
       <h1 text="30px center">
         <RouterLink
           :to="post.path"
+          inline-block
           break-all
           relative
           before:mx-auto
-          before:h-2px
+          before:h-3px
           before:w-0
+          before:inline-block
           before:content-empty
-          before:transition
-          before:inset-x-0
+          before:transition-width
           before:bottom--5px
+          before:left-0
+          before:right-0
           before:absolute
           before:hover:w-full
-          before:hover:bg="[var(--mygo-c-text-1)]"
+          before:bg="[var(--mygo-c-text-1)]"
         >
+          <!-- before:hover:w-full
+          before:hover:bg="[var(--mygo-c-text-1)]" -->
           {{ post.title }}
         </RouterLink>
       </h1>
